ca1aa87213fbe6eb7f1268ad357aa61199050776
Lars Sjöström <lars@radicore.se>
2022-03-07 19:34:31 +0100

    clean up

--- .tmux/plugins/tmux-keyboard/keyboard.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-KBD_LAYOUT=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | sed -E 's/^.+ = \"?([^\"]+)\"?;$/\1/')
-echo ${KBD_LAYOUT:0:3}
