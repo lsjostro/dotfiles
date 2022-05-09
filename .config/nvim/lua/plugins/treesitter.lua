727ec7a1ba0fbc117ce28e90003cd15f1e3dba47
Lars Sjöström <lars@radicore.se>
2022-05-09 20:35:00 +0200

    yes all instead of maintained as its deprecated

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local config = require("nvim-treesitter.configs")
 
 config.setup({
-	ensure_installed = "maintained",
+	ensure_installed = "all",
 	highlight = {
 		enable = true,
 		use_languagetree = true,
