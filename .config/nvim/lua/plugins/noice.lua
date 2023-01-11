59571ab7d48c8905b56c7a830a621aaf8bb0f697
Lars Sjöström <lars@radicore.se>
2023-01-11 14:22:33 +0100

    starship: add git commit to prompt

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,15 +1,6 @@
 local noice = require("noice")
 
 noice.setup({
-	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
-		},
-	},
-
 	-- you can enable a preset for easier configuration
 	presets = {
 		bottom_search = true, -- use a classic bottom cmdline for search
