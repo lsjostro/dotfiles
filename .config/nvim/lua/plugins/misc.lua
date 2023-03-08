056782cf96886805fb30ce5dbf3f29604a6fef98
Lars Sjöström <lars@radicore.se>
2023-03-08 18:36:23 +0100

    nvim: change keymap for comment

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -19,8 +19,8 @@ return {
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "<C-_>", "<Plug>(comment_toggle_linewise_current)" },
-      { "gc",    "<Plug>(comment_toggle_linewise_visual)", mode = "v" },
+      { "<C-/>", "<Plug>(comment_toggle_linewise_current)" },
+      { "<C-/>", "<Plug>(comment_toggle_linewise_visual)", mode = "v" },
     },
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
