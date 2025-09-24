71ff90ecae003e57381db7a63e032379f7f35844
Lars Sjöstrom <lars@radicore.se>
2025-09-24 10:21:06 +0200

    chore: update ssh config and ghostty color scheme

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -2,15 +2,23 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
-    compression = false;
-    controlMaster = "auto";
-    controlPath = "\${XDG_RUNTIME_DIR}/ssh_control:%h:%p:%r";
-    controlPersist = "15m";
+    enableDefaultConfig = false;
     extraConfig = ''
       PreferredAuthentications publickey
       Include ~/.ssh/private_config
     '';
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      compression = false;
+      controlMaster = "auto";
+      controlPath = "\${XDG_RUNTIME_DIR}/ssh_control:%h:%p:%r";
+      controlPersist = "15m";
+      forwardAgent = false;
+      hashKnownHosts = false;
+      serverAliveCountMax = 3;
+      serverAliveInterval = 0;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
   };
 
   home.file = {
