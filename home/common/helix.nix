8a75acab28e6ccd645f533f7d99b74f147d763fc
Lars Sjöstrom <lars@radicore.se>
2026-01-28 14:44:39 +0100

    chore: helix tinkering
    Signed-off-by: Lars Sjöstrom <lars@radicore.se>

--- home/common/helix.nix
+++ home/common/helix.nix
@@ -6,13 +6,57 @@
 
   programs.helix = {
     enable = true;
+
+    extraPackages = with pkgs; [
+      nixd
+      nixfmt
+      taplo
+      asciidoctor
+      vale
+      (pkgs.hunspell.withDicts (dicts: [
+        dicts.en_GB-ize
+        dicts.en_US
+        dicts.sv_SE
+      ]))
+      vscode-langservers-extracted
+      typescript-language-server
+      yaml-language-server
+      typescript
+      nodePackages.prettier
+      bash-language-server
+      marksman
+      tinymist
+      go
+      gotools
+      gopls
+    ];
+
     defaultEditor = true;
     ignores = [
       "!.jj"
     ];
+
+    languages = {
+
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+        }
+        {
+          name = "rust";
+          auto-format = true;
+        }
+      ];
+
+    };
+
     settings = {
-      theme = "alabaster";
+
+      theme = "wolf-alabaster-light-bg";
+
       editor = {
+        auto-pairs = false;
         color-modes = true;
         line-number = "relative";
         cursor-shape = {
@@ -23,133 +67,32 @@
         inline-diagnostics = {
           cursor-line = "hint";
         };
+        popup-border = "all";
         cursorline = true;
         true-color = true;
         undercurl = true;
       };
+
       keys.normal = {
         space.q = ":buffer-close";
+        tab = [ "buffer_picker" ];
       };
+
     };
-    themes.alabaster = {
-      "ui.text" = "fg";
-      "ui.text.focus".bg = "selected";
-      "ui.linenr".fg = "fg_light";
-      "ui.linenr.selected" = {
-        bg = "bg";
-        fg = "fg";
-      };
-      "ui.selection".bg = "selected";
-      "ui.cursorline".bg = "bg_ui";
-      "ui.statusline" = {
-        fg = "status_bar_fg";
-        bg = "status_bar_bg";
-      };
-      "ui.statusline.inactive" = {
-        fg = "inact_status_bar_fg";
-        bg = "inact_status_bar_bg";
-      };
-      "ui.virtual" = "indent";
-      "ui.virtual.ruler".bg = "bg_ui";
-      "ui.virtual.jump-label" = {
-        fg = "fg";
-        bg = "orange";
-        modifiers = [ "bold" ];
-      };
-      "ui.cursor.match".bg = "bg_darker";
-      "ui.cursor" = {
-        bg = "cursor";
-        fg = "white";
-      };
-      "ui.debug".fg = "orange";
-      "ui.highlight.frameline".bg = "#da8581";
-      "ui.help" = {
-        fg = "fg";
-        bg = "bg";
-      };
-      "ui.popup" = {
-        fg = "fg";
-        bg = "bg_ui";
-      };
-      "ui.menu" = {
-        fg = "fg";
-        bg = "bg_menu";
-      };
-      "ui.menu.selected".bg = "selected";
-      "ui.window".bg = "bg_ui";
-      "ui.bufferline" = {
-        fg = "fg";
-        bg = "bg";
-      };
-      "ui.bufferline.active" = {
-        fg = "fg";
-        bg = "bg";
-      };
+  };
 
-      "string" = "string";
-      "comment" = "comment";
-      "function" = "definitions";
-      "constant" = "constants";
-      "punctuation" = "punctuation";
+  xdg.configFile."helix/themes/wolf-alabaster-dark.toml".source = builtins.fetchurl {
+    url = "https://github.com/wolf/alabaster-for-helix/raw/refs/heads/main/helix/dot-config/helix/themes/wolf-alabaster-dark.toml";
+    sha256 = "sha256:0x99k0w6gkmsql12z2j1qrwly1q9rf2kz7b5sn8nmap8d0cacg5s";
+  };
 
-      "error" = {
-        fg = "red";
-        bg = "bg_error";
-      };
-      "warning".fg = "orange";
-      "hint" = {
-        fg = "gray";
-        bg = "bg_hint";
-      };
-      "diagnostic.error" = {
-        fg = "red";
-        bg = "bg_error";
-        underline.style = "curl";
-        modifiers = [ ];
-      };
-      "diagnostic.warning" = {
-        bg = "orange";
-        fg = "fg";
-        modifiers = [ "bold" ];
-      };
-      "diagnostic.hint" = {
-        fg = "gray";
-        modifiers = [ "bold" ];
-      };
-      "diagnostic.unnecessary".modifiers = [ "dim" ];
-      "diagnostic.deprecated".modifiers = [ "crossed_out" ];
+  xdg.configFile."helix/themes/wolf-alabaster-light.toml".source = builtins.fetchurl {
+    url = "https://github.com/wolf/alabaster-for-helix/raw/refs/heads/main/helix/dot-config/helix/themes/wolf-alabaster-light.toml";
+    sha256 = "sha256:0388m2pzzsm75ybyypprkx7m7wb7b5s2vym8rn88k6z8n8635m5a";
+  };
 
-      "diff.plus".fg = "green";
-      "diff.delta".fg = "bg_ui";
-      "diff.minus".fg = "red";
-
-      palette = {
-        white = "#ffffff";
-        bg = "#f7f7f7";
-        bg_darker = "#d0d0d0";
-        bg_ui = "#eeeeee";
-        bg_menu = "#d0d0d0d";
-        bg_error = "#fff0f70";
-        bg_hint = "#eeeeee";
-        fg = "#000000";
-        fg_light = "#9da39a";
-        status_bar_bg = "#777777";
-        status_bar_fg = "#f7f7f7";
-        definitions = "#325CC0";
-        constants = "#7A3E9D";
-        punctuation = "#777777";
-        inact_status_bar_bg = "#DDDDDD";
-        inact_status_bar_fg = "#474747";
-        comment = "#AA3731";
-        string = "#448C27";
-        selected = "#BFDBFE";
-        cursor = "#444444";
-        red = "#a0342f";
-        green = "#065905";
-        indent = "#aaaaaa";
-        orange = "#f0ad4e";
-        gray = "#777777";
-      };
-    };
+  xdg.configFile."helix/themes/wolf-alabaster-light-bg.toml".source = builtins.fetchurl {
+    url = "https://github.com/wolf/alabaster-for-helix/raw/refs/heads/main/helix/dot-config/helix/themes/wolf-alabaster-light-bg.toml";
+    sha256 = "sha256:1044yip99mcilrbj5bb7d355ld4vb17a70gwyj12qq4r3b9m3cvh";
   };
 }
