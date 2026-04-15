3dc9b9d71c7d9e68dcd054fe45df96c9c1ce78b4
Lars Sjöstrom <lars@radicore.se>
2026-04-15 16:04:45 +0200

    chore: dont manage ghostty only config
    Signed-off-by: Lars Sjöstrom <lars@radicore.se>

--- home/common/ghostty.nix
+++ home/common/ghostty.nix
@@ -1,43 +1,28 @@
 {
-  pkgs,
   ...
 }:
 {
-  programs.ghostty = {
-    enable = true;
-    enableFishIntegration = true;
-    systemd.enable = false;
-    installBatSyntax = false;
-    package = pkgs.emptyDirectory;
-
-    settings = {
-      font-size = 11;
-      font-family = "Go Mono";
-
-      adjust-cell-height = 6;
-      adjust-font-baseline = 1;
-      adjust-cursor-thickness = 4;
-      adjust-underline-position = 5;
-      adjust-underline-thickness = -2;
-
-      mouse-hide-while-typing = true;
-      unfocused-split-opacity = 0.9;
-      unfocused-split-fill = "#056157";
-
-      shell-integration = "fish";
-
-      gtk-tabs-location = "hidden";
-      gtk-titlebar = false;
-      window-padding-balance = true;
-      window-padding-color = "extend";
-      window-padding-x = 10;
-      window-padding-y = 10;
-      window-height = 60;
-      window-width = 160;
-      window-theme = "system";
-
-      theme = "light:GitHub Light High Contrast,dark:GitHub Dark High Contrast";
-
-    };
-  };
+  xdg.configFile."ghostty/config".text = ''
+    font-family = Go Mono
+    font-size = 11
+    adjust-cell-height = 6
+    adjust-cursor-thickness = 4
+    adjust-font-baseline = 1
+    adjust-underline-position = 5
+    adjust-underline-thickness = -2
+    gtk-tabs-location = hidden
+    gtk-titlebar = false
+    mouse-hide-while-typing = true
+    shell-integration = fish
+    theme = light:GitHub Light High Contrast,dark:GitHub Dark High Contrast
+    unfocused-split-fill = #056157
+    unfocused-split-opacity = 0.900000
+    window-height = 60
+    window-padding-balance = true
+    window-padding-color = extend
+    window-padding-x = 10
+    window-padding-y = 10
+    window-theme = system
+    window-width = 160
+  '';
 }
